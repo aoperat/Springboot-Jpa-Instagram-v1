62aa7c51a7d515f2ab9c4e322981f448f04c0ffd
ssarmango <ssarmango@gmail.com>
2019-05-30 15:10:19 +0900

    유저 피드 만들기(/explore) mysql8.0 rank() over함수 사용 가능 limit 20 추가

--- src/main/java/com/cos/costagram/repository/ImageRepository.java
+++ src/main/java/com/cos/costagram/repository/ImageRepository.java
@@ -13,6 +13,6 @@ public interface ImageRepository extends JpaRepository<Image, Integer>{
 	public List<Image> findByUserIdOrderByCreateDateDesc(int userId);
 	
 	// not follow imageList (2개씩)
-	@Query(value="select id, caption, createDate, fileName, filePath, location, mimeType, updateDate, userId from (select id, caption, createDate, fileName, filePath, location, mimeType, updateDate, userId, rank() over (partition by userId order by id) as rnk  from image where userId in (select id from user where id not in (select toUser from follow where fromUser = ?1)) and userId != ?1) as img where rnk < 3 order by id desc", nativeQuery = true)
+	@Query(value="select id, caption, createDate, fileName, filePath, location, mimeType, updateDate, userId from (select id, caption, createDate, fileName, filePath, location, mimeType, updateDate, userId, rank() over (partition by userId order by id) as rnk  from image where userId in (select id from user where id not in (select toUser from follow where fromUser = ?1)) and userId != ?1) as img where rnk < 3 order by id desc limit 20", nativeQuery = true)
 	public List<Image> findByNotFollowImageList(int fromUser);
 }
